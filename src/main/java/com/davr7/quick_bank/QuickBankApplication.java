198506652db5080b37cb2a07301d30b50367a55a
Davidson Ratis <davidsonmakarios@gmail.com>
2024-08-18 10:52:13 -0300

    created - first commit

---
+++ src/main/java/com/davr7/quick_bank/QuickBankApplication.java
@@ -0,0 +1,13 @@
+package com.davr7.quick_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davr7/quick_bank/QuickBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davr7.quick_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
