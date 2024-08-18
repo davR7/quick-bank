d2b571aaabc37e2d0e4d594641e521c5f16f8b2c
Davidson Ratis <davidsonmakarios@gmail.com>
2024-08-18 11:46:12 -0300

    updated - handleCreateAccount method to the CustomerResource record

--- src/main/java/com/davr7/quick_bank/resource/CustomerResource.java
+++ src/main/java/com/davr7/quick_bank/resource/CustomerResource.java
@@ -1,8 +1,11 @@
 package com.davr7.quick_bank.resource;
 
 import com.davr7.quick_bank.domain.Customer;
+import com.davr7.quick_bank.dto.CreateAccountDto;
 import com.davr7.quick_bank.dto.CreateCustomerDto;
+import com.davr7.quick_bank.dto.DataAccountDto;
 import com.davr7.quick_bank.dto.DataCustomerDto;
+import com.davr7.quick_bank.service.AccountService;
 import com.davr7.quick_bank.service.CustomerService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "customers")
-public record CustomerResource(CustomerService customerServ) {
+public record CustomerResource(CustomerService customerServ, AccountService accountServ) {
 
     @PostMapping
     public ResponseEntity<DataCustomerDto> handleCreateCustomer(@RequestBody @Valid CreateCustomerDto dto) {
@@ -30,5 +33,11 @@ public record CustomerResource(CustomerService customerServ) {
     public ResponseEntity<Customer> handleFindCustomerByCpf(@PathVariable String cpf) {
         Customer data = customerServ.findCustomerByCpf(cpf);
         return ResponseEntity.ok().body(data);
+    }
+
+    @PostMapping(value = "/{cpf}/accounts")
+    public ResponseEntity<DataAccountDto> handleCreateAccount(@PathVariable String cpf, @RequestBody @Valid CreateAccountDto dto) {
+        DataAccountDto data = accountServ.createAccount(cpf, dto);
+        return ResponseEntity.status(201).body(data);
     }
 }
