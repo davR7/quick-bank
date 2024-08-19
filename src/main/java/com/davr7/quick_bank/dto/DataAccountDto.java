4326516703262c1daf9074c10d437a56674f1da2
Davidson Ratis <davidsonmakarios@gmail.com>
2024-08-19 17:51:35 -0300

    updated - DataAccountDto return only id, accNumber and owner

--- src/main/java/com/davr7/quick_bank/dto/DataAccountDto.java
+++ src/main/java/com/davr7/quick_bank/dto/DataAccountDto.java
@@ -1,14 +1,12 @@
 package com.davr7.quick_bank.dto;
 
 import com.davr7.quick_bank.domain.Account;
-import com.davr7.quick_bank.domain.Customer;
-import com.davr7.quick_bank.domain.enums.AccountType;
 
 import java.util.UUID;
 
-public record DataAccountDto(UUID id, String accNumber, AccountType accType, Double balance, DataCustomerDto owner) {
+public record DataAccountDto(UUID id, String accNumber, DataCustomerDto owner) {
     public static DataAccountDto convert(Account account) {
-        DataCustomerDto owner = new DataCustomerDto(account.getOwner().getId(), account.getOwner().getName(), account.getOwner().getEmail(), account.getOwner().getPhone());
-        return new DataAccountDto(account.getId(), account.getAccNumber(), account.getAccType(), account.getBalance(), owner);
+        DataCustomerDto owner = new DataCustomerDto(account.getOwner().getId(), account.getOwner().getName());
+        return new DataAccountDto(account.getId(), account.getAccNumber(), owner);
     }
 }
