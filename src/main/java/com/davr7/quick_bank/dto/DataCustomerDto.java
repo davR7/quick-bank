ec7e02ecd015fb8380c018a65bab9e665e40a722
Davidson Ratis <davidsonmakarios@gmail.com>
2024-08-19 17:50:11 -0300

    updated - DataCustomerDto return only id and name

--- src/main/java/com/davr7/quick_bank/dto/DataCustomerDto.java
+++ src/main/java/com/davr7/quick_bank/dto/DataCustomerDto.java
@@ -2,11 +2,10 @@ package com.davr7.quick_bank.dto;
 
 import com.davr7.quick_bank.domain.Customer;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
-public record DataCustomerDto(UUID id, String name, String email, String phone) {
+public record DataCustomerDto(UUID id, String name) {
     public static DataCustomerDto convert(Customer customer) {
-        return new DataCustomerDto(customer.getId(), customer.getName(), customer.getEmail(), customer.getPhone());
+        return new DataCustomerDto(customer.getId(), customer.getName());
     }
 }
